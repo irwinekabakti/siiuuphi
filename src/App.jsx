6a65dd3a939ef0eaf17327853bcd7fc5f03030fc
irwinekabakti <irwinekabakti@gmail.com>
2023-05-20 17:57:19 +0700

    chore:(restructure router)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/Routes";
 import {
   Home,
   CategoryProduct,
@@ -13,20 +15,24 @@ import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product/:id" element={<ProductSingle />} />
-          <Route path="/category/:category" element={<CategoryProduct />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/search/:searchTerm" element={<Search />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
+    // <div className="App">
+    //   <BrowserRouter>
+    //     <Header />
+    //     <Sidebar />
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/product/:id" element={<ProductSingle />} />
+    //       <Route path="/category/:category" element={<CategoryProduct />} />
+    //       <Route path="/cart" element={<Cart />} />
+    //       <Route path="/search/:searchTerm" element={<Search />} />
+    //     </Routes>
+    //     <Footer />
+    //   </BrowserRouter>
+    // </div>
+
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 };
 
